573799ea378cb32aeb986e7ef760e1eb7c745616
shayansanjideh <94887104+shayansanjideh@users.noreply.github.com>
2022-07-11 20:49:34 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -25,63 +25,105 @@ function App() {
         const transactions = await Web3Api.account.getTransactions(address);
         // `txs` refers to the individual transactions and their associated metadata
         const txs = transactions.result;
-
         if (txs) {
+            txs.reverse();
             for (let tx of txs) {
-                let net_token_value = 0;
-                let net_eth_value = 0;
-                // If a tx is an ERC-20 token tx.value will be 0
-                if (tx.value === "0") {
-                    let erc20_args = {
-                        address: ethAddress,
-                        to_block: parseInt(tx.block_number),
-                    };
-                    // Retrieve complete list of all ERC-20 tokens in a wallet
-                    let erc20_tokens = await Web3Api.account.getTokenBalances(erc20_args);
+                let portfolio_value: number = 0;
+                console.log(tx);
+                // Outer if loop: if assets are entering the address
+                if (tx.to_address === ethAddress) {
+                    // Inner if loop: if assets are ERC-20 tokens
+                    if (tx.value === "0") {
+                        let erc20_args = {
+                            address: ethAddress,
+                            to_block: parseInt(tx.block_number),
+                        };
+                        // Retrieve complete list of all ERC-20 tokens in a wallet
+                        let erc20_tokens = await Web3Api.account.getTokenBalances(erc20_args);
 
-                    // Loop through the list of ERC-20 tokens
-                    // Here the various metadata is initialized
-                    for (let token of erc20_tokens) {
-                        let token_balance = parseInt(token.balance);
-                        let token_decimals = parseInt(token.decimals);
-                        // let token_logo = token.logo;
-                        // let token_name = token.name;
-                        // let token_symbol = token.symbol;
-                        // let token_thumbnail = token.thumbnail;
-                        let token_address = token.token_address;
+                        // Loop through the list of ERC-20 tokens
+                        // Here the various metadata is initialized
+                        for (let token of erc20_tokens) {
+                            let token_balance = parseInt(token.balance);
+                            let token_decimals = parseInt(token.decimals);
+                            // let token_logo = token.logo;
+                            // let token_name = token.name;
+                            // let token_symbol = token.symbol;
+                            // let token_thumbnail = token.thumbnail;
+                            let token_address = token.token_address;
 
+                            let token_price_args = {
+                                address: token_address,
+                                to_block: parseInt(tx.block_number),
+                            }
+                            // Retrieve the token's price at the block of the transaction
+                            let token_price = await Web3Api.token.getTokenPrice(token_price_args);
+
+                            // Calculate value of a particular token
+                            portfolio_value += token_balance * Math.pow(10, -(token_decimals)) * token_price.usdPrice;
+                        }
+                        // Inner if loop: if transaction involves native ETH
+                } else {
+                        let eth_balance = parseInt(tx.value) * Math.pow(10, -18);
+                        const wETH_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
                         let token_price_args = {
-                            address: token_address,
+                            address: wETH_ADDRESS,
                             to_block: parseInt(tx.block_number),
                         }
                         // Retrieve the token's price at the block of the transaction
-                        let token_price = await Web3Api.token.getTokenPrice(token_price_args);
-
-                        // Calculate value of a particular token
-                        let token_value = token_balance * Math.pow(10, -(token_decimals)) * token_price.usdPrice;
-
-                        net_token_value += token_value;
+                        let eth_price = await Web3Api.token.getTokenPrice(token_price_args);
+                        portfolio_value += eth_balance * eth_price.usdPrice;
                     }
-                    // console.log("list of ERC-20 tokens: ", erc20_tokens);
-                    console.log("net_token_value: ", net_token_value);
-
-                    // If a tx has a non-zero value it is in native ETH
+                    // Outer if loop: if assets are leaving the address
                 } else {
-                    let eth_balance = parseInt(tx.value) * Math.pow(10, -18);
-                    const wETH_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
-                    let token_price_args = {
-                        address: wETH_ADDRESS,
-                        to_block: parseInt(tx.block_number),
+                    // Inner if loop: if assets are ERC-20 tokens
+                    if (tx.value === "0") {
+                        let erc20_args = {
+                            address: ethAddress,
+                            to_block: parseInt(tx.block_number),
+                        };
+                        // Retrieve complete list of all ERC-20 tokens in a wallet
+                        let erc20_tokens = await Web3Api.account.getTokenBalances(erc20_args);
+
+                        // Loop through the list of ERC-20 tokens
+                        // Here the various metadata is initialized
+                        for (let token of erc20_tokens) {
+                            let token_balance = parseInt(token.balance);
+                            let token_decimals = parseInt(token.decimals);
+                            // let token_logo = token.logo;
+                            // let token_name = token.name;
+                            // let token_symbol = token.symbol;
+                            // let token_thumbnail = token.thumbnail;
+                            let token_address = token.token_address;
+
+                            let token_price_args = {
+                                address: token_address,
+                                to_block: parseInt(tx.block_number),
+                            }
+                            // Retrieve the token's price at the block of the transaction
+                            let token_price = await Web3Api.token.getTokenPrice(token_price_args);
+
+                            // Calculate value of a particular token
+                            portfolio_value -= token_balance * Math.pow(10, -(token_decimals)) * token_price.usdPrice;
+                        }
+                        // Inner if loop: if transaction involves native ETH
+                    } else {
+                        let eth_balance = parseInt(tx.value) * Math.pow(10, -18);
+                        const wETH_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+                        let token_price_args = {
+                            address: wETH_ADDRESS,
+                            to_block: parseInt(tx.block_number),
+                        }
+                        // Retrieve the token's price at the block of the transaction
+                        let eth_price = await Web3Api.token.getTokenPrice(token_price_args);
+                        portfolio_value -= eth_balance * eth_price.usdPrice;
                     }
-                    // Retrieve the token's price at the block of the transaction
-                    let eth_price = await Web3Api.token.getTokenPrice(token_price_args);
-                    net_eth_value = eth_balance * eth_price.usdPrice;
-                    console.log("eth_value: ", net_eth_value);
                 }
-                // TODO: Make this logic work somehow?
-                let net_portfolio_value = net_eth_value + net_token_value;
+                console.log(portfolio_value);
             }
         }
+
+
     };
 
     return (
